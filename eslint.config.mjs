653f5e491ab74a95525867805f984783c46f57bf
Ronald_dev <ronaldzamoradev@gmail.com>
2026-01-22 10:23:16 -0500

    setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,43 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  {
+    rules: {
+      'indent': ['error', 2],
+      semi: ['error', 'never'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'comma-dangle': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'space-before-function-paren': ['error', 'never'],
+      'jsx-quotes': ['error', 'prefer-single'],
 
-export default eslintConfig;
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+      'eqeqeq': ['error', 'always'],
+      'no-multi-spaces': 'error',
+      'no-trailing-spaces': 'error',
+      'curly': ['error', 'all'],
+
+      'import/no-duplicates': 'error',
+
+      'react/jsx-boolean-value': ['error', 'never'],
+      'react/self-closing-comp': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'off'
+    }
+  },
+
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts'
+  ])
+])
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className='h-screen w-screen flex justify-center items-center'>
+      <h1 className='font-bold text-xl'>Hello world! 🎉</h1>
+    </main>
+  )
+}
